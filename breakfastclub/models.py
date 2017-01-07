a56afc9af102cda12c3d95e50738420626c1bdbc
Jesper Sindahl Nielsen <jesper.sindahl.nielsen@gmail.com>
2017-01-07 11:09:00 +0100

    project organization

---
+++ breakfastclub/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('mysql+mysqlconnector://breakfast:monster@localhost/breakfastclub')
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+class Person(Base):
+    __tablename__ = 'people'
+
+    personId = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    email = Column(String(200))
+
+    def __repr__(self):
+        return '<Person(name=%s, email=%s)>' % (name, email)
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='breakfastclub',
+    packages=['breakfastclub'],
+    include_package_data=True,
+)
