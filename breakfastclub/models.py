0990a171d409d5cf4dc52de8f49da1d927e8ed2b
Jesper Sindahl Nielsen <jesper.sindahl.nielsen@gmail.com>
2017-01-14 16:48:32 +0100

    some work

--- breakfastclub/models.py
+++ breakfastclub/models.py
@@ -1,6 +1,15 @@
-from sqlalchemy import create_engine, Column, Integer, String
+import datetime
+from random import shuffle
+
+from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import ForeignKey
+from sqlalchemy import func
+
+import click
+
+from breakfastclub import app
 
 engine = create_engine('mysql+mysqlconnector://breakfast:monster@localhost/breakfastclub')
 Base = declarative_base()
@@ -9,9 +18,67 @@ Session = sessionmaker(bind=engine)
 class Person(Base):
     __tablename__ = 'people'
 
-    personId = Column(Integer, primary_key=True)
+    person_id = Column(Integer, primary_key=True)
     name = Column(String(100))
     email = Column(String(200))
+    active = Column(Boolean)
+    def __repr__(self):
+        return '<Person(name=%s, email=%s)>' % (self.name, self.email)
+
+
+class breadlists(Base):
+    __tablename__ = 'breadlists'
+
+    id = Column(Integer, primary_key=True)
+
+
+class bread_bringer(Base):
+    __tablename__ = 'breadbringer'
+
+    entry_id = Column(Integer, primary_key=True)
+    breadlist_id = Column(Integer, ForeignKey('breadlists.id'))
+    person_id = Column(Integer, ForeignKey('people.person_id'))
+    date = Column(Date)
 
     def __repr__(self):
-        return '<Person(name=%s, email=%s)>' % (name, email)
+        return '<BreadlistEntry(person_id=%s, date=%s)>' % (self.person_id,
+                                                            self.date)
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initialize the database."""
+    Base.metadata.create_all(engine)
+    print("Database initialized")
+
+
+@app.cli.command('generate-breadlist')
+@click.option('--day', default=1)
+# 1 is tuesday, following python date.weekday()
+def generate_breadlist(day):
+    """
+    Generates a new breadlist.
+    """
+    session = Session()
+    active_people = session.query(Person).filter_by(active=True).all()
+    shuffle(active_people)
+    today = datetime.date.today()
+
+    week_length = 7
+    if today.weekday() >= day:
+        # next week has next breakfast day.
+
+        days_to_add = week_length - (today.weekday() - day)
+        pass
+    else:
+        # this week has next breakfast day.
+        days_to_add = day - today.weekday()
+    next_breakfast_date = (datetime.date.today() +
+                           datetime.timedelta(days=days_to_add))
+    breakfast_dates = [(next_breakfast_date +
+                        datetime.timedelta(week_length * i))
+                       for i, _ in enumerate(active_people)]
+
+
+    for person, date in zip(active_people, breakfast_dates):
+        print("[%s] %s (%s)" % (date, person.name, person.email))
--- breakfastclub/views.py
+++ breakfastclub/views.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
+
+
 from breakfastclub import app
 from breakfastclub.models import Person, Session
-import click
+
 
 @app.route('/')
 def index():
@@ -13,23 +15,29 @@ def show_people():
     peeps = session.query(Person.name, Person.email)
     return render_template('people.html', people=peeps)
 
-@app.cli.command('initdb')
-def initdb_command():
-    """Initialize the database."""
-    # TODO: fix this. Should be placed somewhere else.
-    #people.Base.metadata.create_all(people.engine)
-    print("Database initialized")
 
 @app.route('/people/add', methods=['POST', 'GET'])
 def add_person():
     if request.method == 'POST':
+        print("Hello")
         name = str(request.form['name'])
         email = str(request.form['email'])
-        person = Person(name=name, email=email)
-        session = people.Session()
+        person = Person(name=name, email=email, active=True)
+        session = Session()
         session.add(person)
         session.commit()
-        return render_template('add_person.html')#, person_added=True)
+        return show_people() # render_template('add_person.html')#, person_added=True)
     else:
         return render_template('add_person.html')#, person_added=False)
 
+
+@app.route('/breadlist')
+def show_breadlist():
+    breadbringers = [{'date': '20-01-2017',
+                      'name': 'Jesper',
+                      'email': 'xyz@cs.au.dk'},
+                     {'date': '27-01-2017',
+                      'name': 'Gerth',
+                      'email': 'abc@cs.au.dk'},]
+
+    return render_template('breadlist.html', breadbringers=breadbringers)
