40e0722fd2fa36c440f310f1eb96a65784aa9784
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2017-08-02 16:22:57 +0200

    make relationship between breadlist and person backref

--- breakfastclub/models.py
+++ breakfastclub/models.py
@@ -15,9 +15,23 @@ class Person(db.Model, UserMixin):
     active = db.Column(db.Boolean, default=True, nullable=False)
     token = db.Column(db.String(128))
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    breadlist_set = db.relationship('BreadList', backref='person', lazy='dynamic')
 
     def __repr__(self):
         return '<Person(name=%s, email=%s)>' % (self.name, self.email)
+
+    def __str__(self):
+        return self.name
+
+# For some strange reason the below code doesn't always
+# replace the anonymous_user default with AnonymousPerson
+# Maybe related to https://github.com/maxcountryman/flask-login/issues/261
+#
+# class AnonymousPerson(AnonymousUserMixin):
+#     is_admin = False
+
+# login_manager.anonymous_user = AnonymousPerson
+
 
 class BreadList(db.Model):
     __tablename__ = 'breadlist'
@@ -58,3 +72,4 @@ def generate_breadlist(day):
 
     for person, date in zip(active_people, breakfast_dates):
         print("[%s] %s (%s)" % (date, person.name, person.email))
+
