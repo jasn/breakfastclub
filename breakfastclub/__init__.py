42e526bc2402c18ac900c4554a50ed34bd0ade2c
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2017-08-03 13:39:43 +0200

    A command for sending a reminder email

--- breakfastclub/__init__.py
+++ breakfastclub/__init__.py
@@ -1,21 +1,37 @@
+import json
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
+from flask_mail import Mail
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://breakfast:monster@localhost/breakfastclub'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '13'
+
+# read email config
+this_directory = os.path.dirname(__file__)
+email_config_path = os.path.join(this_directory, 'email_config.json')
+with open(email_config_path) as email_config_file:
+    app.config.update(json.load(email_config_file))
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 csrf = CSRFProtect(app)
+mail = Mail(app)
 
 from . import models
 from . import views
 from . import admin
+from . import commands
+
 
 @login_manager.user_loader
 def load_user(person_id):
---
+++ breakfastclub/commands.py
@@ -0,0 +1,39 @@
+import datetime
+
+import click
+
+from flask_mail import Message
+
+from breakfastclub import app, db, mail
+from breakfastclub.models import Person, BreadList
+
+
+@app.cli.command('send-mail-reminder')
+@click.option('-n', '--dry-run', is_flag=True)
+# 1 is tuesday, following python date.weekday()
+def send_mail_reminder(dry_run):
+    text_message = """Hello {name}.
+You are bringing breakfast on {date}.
+There are currently {count} people participating in the breakfastclub.
+Best regards,
+The Breakfastclub
+"""
+    subject = "[Breakfastclub] Reminder"
+    today = datetime.date.today()
+    qs = db.session.query(BreadList).filter(BreadList.date >= today)
+    qs = qs.order_by(BreadList.date)
+    up_next = qs.first()
+
+    qs = db.session.query(Person)
+    qs = qs.filter(Person.active == True)
+    count = qs.count()
+
+    text_message = text_message.format(name=up_next.person.name,
+                                       date=up_next.date,
+                                       count=count)
+    email_message = Message(text_message, sender='breakfastclub@example.com',
+                            recipients=[up_next.person.email])
+    if dry_run:
+        print(email_message)
+    else:
+        mail.send(email_message)
