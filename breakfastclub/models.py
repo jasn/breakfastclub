6ab6622da1f71da1128b8a897c45a6d883c2814f
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2017-08-03 13:36:53 +0200

    dead code removal.

--- breakfastclub/models.py
+++ breakfastclub/models.py
@@ -42,34 +42,3 @@ class BreadList(db.Model):
                           db.ForeignKey(Person.id, name='fk_breadlist_person_id'),
                           nullable=False)
 
-
-@app.cli.command('generate-breadlist')
-@click.option('--day', default=1)
-# 1 is tuesday, following python date.weekday()
-def generate_breadlist(day):
-    """
-    Generates a new breadlist.
-    """
-    active_people = db.session.query(Person).filter_by(active=True).all()
-    shuffle(active_people)
-    today = datetime.date.today()
-
-    week_length = 7
-    if today.weekday() >= day:
-        # next week has next breakfast day.
-
-        days_to_add = week_length - (today.weekday() - day)
-        pass
-    else:
-        # this week has next breakfast day.
-        days_to_add = day - today.weekday()
-    next_breakfast_date = (datetime.date.today() +
-                           datetime.timedelta(days=days_to_add))
-    breakfast_dates = [(next_breakfast_date +
-                        datetime.timedelta(week_length * i))
-                       for i, _ in enumerate(active_people)]
-
-
-    for person, date in zip(active_people, breakfast_dates):
-        print("[%s] %s (%s)" % (date, person.name, person.email))
-
